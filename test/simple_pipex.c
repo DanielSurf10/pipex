2a5c1fe3dc4a016e670b1b6b3c4ce68719b3f034
DanielSurf10 <daniel101surf@gmail.com>
2024-02-12 11:41:04 -0300

    mexendo

--- test/simple_pipex.c
+++ test/simple_pipex.c
@@ -6,7 +6,7 @@
 /*   By: danbarbo <danbarbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 17:12:50 by danbarbo          #+#    #+#             */
-/*   Updated: 2024/02/11 23:47:21 by danbarbo         ###   ########.fr       */
+/*   Updated: 2024/02/12 11:39:52 by danbarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,24 @@ int main(int argc, char *argv[], char *envp[])
 	int	fd_file_in;
 	int	fd_file_out;
 	int	fd[2];
-	int	pid[2];
+	int	fd2[2];
+	int	pid[3];
 
-	if (argc != 5)
+	if (argc != 6)
 	{
-		write(2, "Usage error.\nExpected: ./pipex <file_in> <cmd> <cmd> <file_out>\n", 64);
+		write(2, "Usage error.\nExpected: ./pipex <file_in> <cmd> <cmd> <cmd> <file_out>\n", 71);
 		return (1);
 	}
 
 	fd_file_in = open(argv[1], O_RDONLY);
 
+	printf("%s\n", argv[1]);
+	printf("%s\n", argv[5]);
+
 	if (fd_file_in < 0)
 		perror("Input file error");
 
-	fd_file_out = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	fd_file_out = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
 	if (fd_file_out < 0)
 		perror("Output file error");
@@ -45,7 +49,7 @@ int main(int argc, char *argv[], char *envp[])
 
 	if (pid[0] == 0)
 	{
-		// Processo filho
+		// Processo filho primeiro comando
 		close(fd[0]);
 		close(fd_file_out);
 
@@ -63,30 +67,59 @@ int main(int argc, char *argv[], char *envp[])
 		exit(2);
 	}
 
+	pipe(fd2);
 	pid[1] = fork();
 
 	if (pid[1] == 0)
 	{
-		// Processo filho
+		// Processo filho segundo comando
 		close(fd[1]);
+		close(fd2[0]);
 		close(fd_file_in);
-
-		char *args[] = {"/bin/head", "-1", NULL};
-
-		dup2(fd[0], STDIN_FILENO);
-		dup2(fd_file_out, STDOUT_FILENO);
-
-		close(fd[0]);
 		close(fd_file_out);
 
+		char *args[] = {argv[3], NULL};
+
+		dup2(fd[0], STDIN_FILENO);
+		dup2(fd2[1], STDOUT_FILENO);
+
+		close(fd[0]);
+		close(fd2[1]);
+
 		if (!(fd_file_in < 0) && !(fd_file_out < 0))
-			execve("/bin/head", args, envp);
+			execve(argv[3], args, envp);
 		write(2, "Deu ruim 2\n", 11);
 		exit(2);
 	}
 
 	close(fd[0]);
 	close(fd[1]);
+
+	pid[3] = fork();
+
+	if (pid[3] == 0)
+	{
+		// Terceiro comando
+		close(fd2[1]);
+		close(fd_file_in);
+
+		char *args[] = {argv[4], NULL};
+
+		dup2(fd2[0], STDIN_FILENO);
+		dup2(fd_file_out, STDOUT_FILENO);
+
+		close(fd2[0]);
+		close(fd_file_out);
+
+		if (!(fd_file_in < 0) && !(fd_file_out < 0))
+			execve(argv[4], args, envp);
+		write(2, "Deu ruim 3\n", 11);
+		exit(2);
+	}
+
+	close(fd2[0]);
+	close(fd2[1]);
+
 	if (!(fd_file_in < 0))
 		close(fd_file_in);
 	if (!(fd_file_out < 0))
@@ -94,6 +127,7 @@ int main(int argc, char *argv[], char *envp[])
 
 	waitpid(pid[0], NULL, 0);
 	waitpid(pid[1], NULL, 0);
+	waitpid(pid[2], NULL, 0);
 
 	return (0);
 }
