8cad92b99ee98807ff842f3c0bdbbd41f74ca0f2
DanielSurf10 <daniel101surf@gmail.com>
2024-02-28 22:39:21 -0300

    novos testes

--- my_tester/tester.py
+++ my_tester/tester.py
@@ -14,9 +14,10 @@ for i in a:
 # print(*commands, sep='\n')
 
 os.system("cp ../pipex .")
+val = "valgrind -q --trace-children=yes --track-fds=yes --track-origins=yes --leak-check=full --show-leak-kinds=all --trace-children-skip='*/bin/*,*/sbin/*' "
 
 for command in commands:
 	print(command)
-	return_code = os.system(command)
+	return_code = os.system(val + command)
 	print((return_code >> 8) & 0xFF)
 	input()
